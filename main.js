5806e2d2879282348c7c285176ba9748d3a9c039
Logan King <59426296+EagleAim@users.noreply.github.com>
2023-11-01 23:05:45 -0700

    final

---
+++ main.js
@@ -0,0 +1,12 @@
+let myGreeting = "Hello World!";
+console.log(myGreeting); //Hello World!
+
+myGreeting = "Salutations World!";
+console.log(myGreeting); //Salutations World!
+
+
+pi = 3.14; 
+// it will keep erroring because it can not change once it is set
+
+let myNumber = "38";
+Math.min(myNumber, pi); //cant seem to get it to work?
